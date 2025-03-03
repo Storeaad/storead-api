626fc701f9689ddd54461dbe1c361a84aaf5f2c1
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-03-04 00:07:44 +0900

    feat: github social login

---
+++ src/main/kotlin/com/storead/auth/client/auth/dto/GithubAccountResponse.kt
@@ -0,0 +1,23 @@
+package com.storead.auth.client.auth.dto
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import com.storead.auth.application.request.AuthServiceRequest
+import com.storead.auth.domain.PlatformType
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class GithubAccountResponse(
+    val name: String,
+    val id: String,
+) {
+    fun toServiceRequest(): AuthServiceRequest {
+        return AuthServiceRequest(
+            name = name,
+            email = "",
+            platformId = id,
+            platform = PlatformType.GOOGLE)
+    }
+}
