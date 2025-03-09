c2c761e6c3253e33215103002f5ad24c8b76e930
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-03-09 21:49:27 +0900

    update: change string image file type to multipart file

--- src/main/kotlin/com/storead/profile/web/request/ProfileUpdateRequest.kt
+++ src/main/kotlin/com/storead/profile/web/request/ProfileUpdateRequest.kt
@@ -1,16 +1,17 @@
 package com.storead.profile.web.request
 
 import com.storead.profile.application.request.ProfileServiceUpdateRequest
+import org.springframework.web.multipart.MultipartFile
 
 data class ProfileUpdateRequest(
     val name: String? = null,
     val aboutMe: String? = null,
-    val image: String? = null,
+    val image: MultipartFile? = null,
 ) {
     fun toServiceRequest(userId: Long) = ProfileServiceUpdateRequest(
         userId = userId,
         name = name,
         aboutMe = aboutMe,
-        image = image
+        imageFile = image
     )
 }
