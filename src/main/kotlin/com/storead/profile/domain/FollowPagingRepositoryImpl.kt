b90c4b0ba4e68c1c43f8341f4e8db50e106df2b8
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-03-23 16:40:22 +0900

    fix: change the join field

--- src/main/kotlin/com/storead/profile/domain/FollowPagingRepositoryImpl.kt
+++ src/main/kotlin/com/storead/profile/domain/FollowPagingRepositoryImpl.kt
@@ -15,14 +15,14 @@ class FollowPagingRepositoryImpl(
     ): List<Follow> {
         val follow = QFollow.follow
 
-        var condition = follow.to.id.eq(profileId)
+        var condition = follow.from.id.eq(profileId)
 
         if (cursor != null) {
             condition = condition.and(follow.id.lt(cursor))
         }
 
         return queryFactory.selectFrom(follow)
-            .join(follow.from).fetchJoin()
+            .join(follow.to).fetchJoin()
             .where(condition)
             .orderBy(follow.id.desc())
             .limit(limit.toLong())
@@ -32,14 +32,14 @@ class FollowPagingRepositoryImpl(
     override fun findFollowersByToId(profileId: Long, limit: Int, cursor: Long?): List<Follow> {
         val follow = QFollow.follow
 
-        var condition = follow.from.id.eq(profileId)
+        var condition = follow.to.id.eq(profileId)
 
         if (cursor != null) {
             condition = condition.and(follow.id.lt(cursor))
         }
 
         return queryFactory.selectFrom(follow)
-            .join(follow.to).fetchJoin()
+            .join(follow.from).fetchJoin()
             .where(condition)
             .orderBy(follow.id.desc())
             .limit(limit.toLong())
