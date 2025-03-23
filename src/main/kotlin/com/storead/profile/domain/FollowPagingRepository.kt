5d2aa7c059dbfdea2772b5ee474f280dbb52265a
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-03-23 16:40:00 +0900

    update: remove the unused import

--- src/main/kotlin/com/storead/profile/domain/FollowPagingRepository.kt
+++ src/main/kotlin/com/storead/profile/domain/FollowPagingRepository.kt
@@ -1,7 +1,5 @@
 package com.storead.profile.domain
 
-import java.util.*
-
 interface FollowPagingRepository {
     fun findFollowingByFromId(profileId: Long, limit: Int, cursor: Long? = null): List<Follow>
     fun findFollowersByToId(profileId: Long, limit: Int, cursor: Long? = null): List<Follow>
