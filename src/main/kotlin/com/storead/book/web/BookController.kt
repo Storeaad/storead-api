e31f1759654d955a388cff6262695fd13a4e9d66
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-04-01 19:26:06 +0900

    update: book web part add docstring

--- src/main/kotlin/com/storead/book/web/BookController.kt
+++ src/main/kotlin/com/storead/book/web/BookController.kt
@@ -16,12 +16,28 @@ class BookController(
     private val bookService: BookService,
 ) {
 
+    /**
+     * 책 정보 등록
+     *
+     * 책 정보를 입력할 시 목차 정보를 수집 하고 책 정보를 등록 함.
+     *
+     * @param request 등록할 책의 정보(isbn, title, author, publishedDate, description, thumbnailUrl)
+     * @return 책 정보 등록 결과
+     */
     @PostMapping
     fun create(request: BookCreateRequest): ResponseEntity<ApiResponse<BookResponse>> {
         val response: BookResponse = bookService.create(request.toServiceRequest()).toResponse()
         return ApiResponse.success(data = response, "${response.title} 도서가 정상적으로 등록 되었습니다.", HttpStatus.CREATED)
     }
 
+    /**
+     * 책 조회
+     *
+     * 책의 고유한 아이디를 입력 하면 해당 책의 정보를 반환 함
+     *
+     * @param book: 책의 UUID 36자리
+     * @return 책 정보 조회 결과
+     */
     @GetMapping("/{book}")
     fun getBook(
         @PathVariable book: UUID,
