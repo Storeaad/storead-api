346cfa848d40334081aae3ae694dbfc4a8f91dd4
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-04-18 00:53:26 +0900

    refactor: replace string with ULID for redis entity

--- src/main/kotlin/com/storead/auth/domain/RefreshTokenRepository.kt
+++ src/main/kotlin/com/storead/auth/domain/RefreshTokenRepository.kt
@@ -1,7 +1,8 @@
 package com.storead.auth.domain
 
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
-    fun findByUserId(userId: Long): RefreshToken?
+interface RefreshTokenRepository : CrudRepository<RefreshToken, UUID> {
+    fun findByUserId(userId: UUID): RefreshToken?
 }
