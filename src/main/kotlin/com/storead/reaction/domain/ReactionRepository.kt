fb8ec921e6de9a7191fc9e4d2cb8f5b9bcff5df0
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-07-20 15:49:35 +0900

    comment: remove comments in ReactionRepository methods

--- src/main/kotlin/com/storead/reaction/domain/ReactionRepository.kt
+++ src/main/kotlin/com/storead/reaction/domain/ReactionRepository.kt
@@ -2,23 +2,12 @@ package com.storead.reaction.domain
 
 import com.storead.common.domain.EntityType
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import java.util.*
 
 interface ReactionRepository : JpaRepository<Reaction, UUID> {
-
-    /**
-     * 사용자가 반응한 모든 내역을 조회합니다.
-     */
     fun findAllByUserId(userId: UUID): List<Reaction>
 
-    /**
-     * 특정 주체에 달린 모든 반응을 조회합니다. (반응 개수 집계 등에 사용)
-     */
     fun findAllBySubjectIdAndSubjectType(subjectId: UUID, subjectType: EntityType): List<Reaction>
 
-    /**
-     * 특정 주체가 삭제될 때, 관련된 모든 반응을 삭제합니다.
-     */
     fun deleteAllBySubjectIdAndSubjectType(subjectId: UUID, subjectType: EntityType)
-
 }
